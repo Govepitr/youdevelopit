c11b66c011db8fe575c2300f364b7b5040d7d424
Dan Arbelo <govepitr@gmail.com>
2021-02-28 16:51:23 -0700

    Does it work? Are the instructions broken? Find out on the next episode of Dragon Ball Z!

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO candidates (first_name, last_name, industry_connected)
+VALUES
+  ('James', 'Howlett', 1),
+  ('Ororo', 'Monroe', 1),
+  ('Remy', 'LeBeua', 0),
+  ('Charles', 'Xavier', 1),
+  ('Cain', 'Marko', 1),
+  ('Dora', 'Explorer', 0),
+  ('Kurt', 'Wargner', 0),
+  ('Nathan', 'Summers', 1),
+  ('Jackie', 'Estacado', 1),
+  ('Matthew', 'Murdock', 1);
