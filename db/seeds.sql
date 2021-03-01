6d3e71e02427e86672c00f813b11d2cc3a67215a
Dan Arbelo <govepitr@gmail.com>
2021-02-28 17:00:01 -0700

    seed and schema

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,12 @@
 INSERT INTO candidates (first_name, last_name, industry_connected)
 VALUES
-  ('James', 'Howlett', 1),
-  ('Ororo', 'Monroe', 1),
-  ('Remy', 'LeBeua', 0),
-  ('Charles', 'Xavier', 1),
-  ('Cain', 'Marko', 1),
-  ('Dora', 'Explorer', 0),
-  ('Kurt', 'Wargner', 0),
-  ('Nathan', 'Summers', 1),
-  ('Jackie', 'Estacado', 1),
-  ('Matthew', 'Murdock', 1);
+  ('Ronald', 'Firbank', 1),
+  ('Virginia', 'Woolf', 1),
+  ('Piers', 'Gaveston', 0),
+  ('Charles', 'LeRoi', 1),
+  ('Katherine', 'Mansfield', 1),
+  ('Dora', 'Carrington', 0),
+  ('Edward', 'Bellamy', 0),
+  ('Montague', 'Summers', 1),
+  ('Octavia', 'Butler', 1),
+  ('Unica', 'Zurn', 1);
